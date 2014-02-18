f28c3b4fa1b7cec951885ec0af71d1882dc2a6cd
YX <yxxinyuan@zju.edu.cn>
2014-02-18 09:57:55 +0800

    add swap

--- public/include/GkcBase.h
+++ public/include/GkcBase.h
@@ -19,12 +19,32 @@ This file contains basic classes.
 #define __GKC_BASE_H__
 ////////////////////////////////////////////////////////////////////////////////
 
+#ifndef __GKC_DEF_H__
+	#error GkcBase.h requires GkcDef.h to be included first.
+#endif
 
 ////////////////////////////////////////////////////////////////////////////////
 namespace GKC {
 ////////////////////////////////////////////////////////////////////////////////
 
+//functions
 
+//Swap
+template <typename T>
+inline void Swap(T& t1, T& t2)
+{
+	assert( &t1 != &t2 );
+	T tmp = static_cast<T&&>(t1);
+	t1 = static_cast<T&&>(t2);
+	t2 = static_cast<T&&>(tmp);
+}
+
+template <>
+inline void Swap<int>(int& t1, int& t2)
+{
+	assert( &t1 != &t2 );
+	t1 ^= t2 ^= t1 ^= t2;
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 }

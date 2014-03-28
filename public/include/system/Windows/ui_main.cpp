64431f49cb3ea3e95487378c7e92f6028c9559aa
YX <yxxinyuan@zju.edu.cn>
2014-03-29 01:15:14 +0800

    implement ui_main

--- public/include/system/Windows/ui_main.cpp
+++ public/include/system/Windows/ui_main.cpp
@@ -28,12 +28,27 @@ int APIENTRY wWinMain(IN HINSTANCE hInstance,
 		::MessageBoxW(NULL, L"Cannot initialize STA!", L"Error", MB_OK);
 		return -1;
 	}
+	// this resolves ATL window thunking problem when Microsoft Layer for Unicode (MSLU) is used
+	::DefWindowProcW(NULL, 0, 0, 0);
+	{
+		INITCOMMONCONTROLSEX iccx = { sizeof(INITCOMMONCONTROLSEX), ICC_COOL_CLASSES | ICC_BAR_CLASSES };
+		BOOL bRet = ::InitCommonControlsEx(&iccx);
+		if( !bRet ) {
+			::CoUninitialize();
+			::MessageBoxW(NULL, L"Cannot initialize common controls!", L"Error", MB_OK);
+			return -1;
+		}
+	} //end block
 
+//command and ShowFlag
 	int ret = ProgramEntryPoint::UIMain();
 
 	::CoUninitialize();
 
 	return ret;
 }
+
+//others
+#include "_Module.cpp"
 
 ////////////////////////////////////////////////////////////////////////////////

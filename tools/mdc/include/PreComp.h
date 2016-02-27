72a83ece113f9027fcbc34b82a1585ecda2c1109
YX <yxxinyuan@zju.edu.cn>
2016-02-27 23:17:59 +0800

    Add

---
+++ tools/mdc/include/PreComp.h
@@ -0,0 +1,31 @@
+﻿/*
+** Copyright (c) 2015, Xin YUAN, courses of Zhejiang University
+** All rights reserved.
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the 2-Clause BSD License.
+**
+** Author contact information:
+**   yxxinyuan@zju.edu.cn
+**
+*/
+
+/*
+This is pre-compiled header file.
+*/
+
+////////////////////////////////////////////////////////////////////////////////
+#ifndef __PRECOMP_H__
+#define __PRECOMP_H__
+////////////////////////////////////////////////////////////////////////////////
+
+#include "base/GkcDef.h"
+#include "base/GkcColl.h"
+
+#include "sys/GkcSys.h"
+#include "sys/GkcConsole.h"
+#include "sys/GkcPath.h"
+
+////////////////////////////////////////////////////////////////////////////////
+#endif //__PRECOMP_H__
+////////////////////////////////////////////////////////////////////////////////
